48810e975cc65da7ce1959aef07c040415825c50
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-11-21 15:47:21 -0500

    Create version.tf

---
+++ k3s_deployment/modules/vm/version.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    proxmox = {
+      source = "telmate/proxmox"
+      version = "2.9.14"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.4"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.3.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.4.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2.1"
+    }
+    external = {
+      source  = "hashicorp/external"
+      version = "~> 2.3.1"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url          = var.api_url
+  pm_api_token_id     = var.api_token_id
+  pm_api_token_secret = var.api_token_secret
+  pm_tls_insecure     = true
+  pm_debug            = true
+  pm_log_enable       = true
+  pm_log_file         = "terraform-plugin-proxmox.log"
+  pm_log_levels = {
+    _default    = "debug"
+    _capturelog = ""
+  }
+}
