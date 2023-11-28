eeb5d17d8d20b521544e39b764e991f6dc66a9c1
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-11-28 07:43:59 -0500

    Update build_cloud_image_vm_template.sh

--- k3s_deployment/modules/pm_vm_template/build_cloud_image_vm_template.sh
+++ k3s_deployment/modules/pm_vm_template/build_cloud_image_vm_template.sh
@@ -21,13 +21,14 @@
 #    Use jq to parse the JSON and extract the value for id 8200
 #    result=$(echo "$json" | jq -r --arg vmid "$VMID" '.ids[$vmid]')
 #---------------------------
-URL="https://cloud-images.ubuntu.com/lunar/current/lunar-server-cloudimg-amd64-disk-kvm.img"
+# Ubuntu 22.04 Cloud Image
+URL="https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64-disk-kvm.img"
 CLOUD_IMG_ORIG=$(basename "$URL")
 CLOUD_IMG=$(echo $CLOUD_IMG_ORIG | sed 's/\.img$/.qcow2/')
 
 VMID="9900"
-TMPL_NAME="ubun-2304-tmpl-01"
-TMPL_DESCRIPTION="Ubuntu 23.04 Cloud Image"
+TMPL_NAME="ubun-2204-k3s-tmpl-01"
+TMPL_DESCRIPTION="Ubuntu 22.04 Cloud Image for K3S Cluster"
 PVE_DISK="fast0-pve6"
 DISK_SZ="32"
 PVE_NODE="pve-6"
