399b2ef1158699637e56306a68868bc9c1b49125
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-11-29 18:20:47 -0500

    Update onboarding_script.sh

--- k3s_deployment/modules/k3s/azure_arc/onboarding_script.sh
+++ k3s_deployment/modules/k3s/azure_arc/onboarding_script.sh
@@ -12,3 +12,24 @@ az provider show -n Microsoft.ExtendedLocation | jq .registrationState
 
 # Create connected cluster
 az connectedk8s connect --name "K3S_Cluster_0" --resource-group "sec_telem_law_1" --location "eastus" --correlation-id "c18ab9d0-685e-48e7-ab55-12588447b0ed" --tags "K3S_Cluster_@Home"
+
+# ----------------------------------- K3S Cluster Successfully Onboarded on Azure Arc ---------------------------------------
+# Create a JWT token so the K3S cluster is visable from Azure Arc
+kubectl create serviceaccount demo-user -n default
+
+kubectl create clusterrolebinding demo-user-binding --clusterrole cluster-admin --serviceaccount default:demo-user
+
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: demo-user-secret
+  annotations:
+    kubernetes.io/service-account.name: demo-user
+type: kubernetes.io/service-account-token
+EOF
+
+TOKEN=$(kubectl get secret demo-user-secret -o jsonpath='{$.data.token}' | base64 -d | sed 's/$/\n/g')
+
+# Copy / Paste the token into the box within Azure Arc
+echo $TOKEN
